fc11a39292e9f50be3691ba06743ed79d6e2253c
Ritoru <92292237+MaximeSTAJSZCZYK@users.noreply.github.com>
2021-11-18 11:50:56 +0100

    Day 2

--- js/script.js
+++ js/script.js
@@ -2,21 +2,6 @@ $(document).ready(function(){
   $('.sidenav').sidenav();
 });
 
-   $(document).ready(function(){
-     $('.parallax').parallax();
-   });
-
-document.addEventListener('DOMContentLoaded', function() {
-  var elems = document.querySelectorAll('.sidenav');
-  var instances = M.Sidenav.init(elems, options);
-});
-
-  // Initialize collapsible (uncomment the lines below if you use the dropdown variation)
-  // var collapsibleElem = document.querySelector('.collapsible');
-  // var collapsibleInstance = M.Collapsible.init(collapsibleElem, options);
-
-  // Or with jQuery
-
-$(document).ready(function(){
-  $('.sidenav').sidenav();
-});
+ $(document).ready(function(){
+   $('.parallax').parallax();
+ });
